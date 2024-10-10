9d6fd42f61aee6d12945cb706fe50850f82aae65
RasulKuchugulov <rkuchugulov@gmail.com>
2024-10-10 18:08:47 +0500

    Add files via upload

---
+++ modul_1_4.py Kuchugulov_Rasul.py
@@ -0,0 +1,8 @@
+my_string = input('Введите кличку вашего кота: ')
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace('',''))
+print(my_string.replace('Мурзик','Вася'))
+print(my_string[0])
+print(my_string[-1])
+
